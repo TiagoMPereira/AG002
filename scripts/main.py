b42b623adce73ffbc182ae63556214666ac8e589
Tiago Pereira <tiagodemoraispereira@gmail.com>
2022-06-12 22:44:21 -0300

    fix: minor fixes

--- scripts/main.py
+++ scripts/main.py
@@ -4,7 +4,8 @@ import pandas as pd
 
 # Criação e avaliação de modelos
 from sklearn.tree import DecisionTreeClassifier
-from sklearn.neighbors import KNeighborsClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.svm import SVC
 from sklearn.model_selection import train_test_split 
 from sklearn.metrics import accuracy_score
 
@@ -22,21 +23,25 @@ y = dataset.iloc[:,9]
 # Dividindo conjunto de treino e teste
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.2)
 
-# Criando modelos de Árvore de decisão e KNN
+# Criando modelos de classificação
 tree = DecisionTreeClassifier()
-knn = KNeighborsClassifier()
+rf = RandomForestClassifier()
+svc = SVC()
 
 # Treinando modelos
 tree.fit(x_train, y_train)
-knn.fit(x_train, y_train)
+rf.fit(x_train, y_train)
+svc.fit(x_train, y_train)
 
 # Fazendo predições com os modelos
 tree_prediction = tree.predict(x_test)
-knn_prediction = knn.predict(x_test)
+rf_prediction = rf.predict(x_test)
+svc_prediction = svc.predict(x_test)
 
 # Avaliando acurácia dos modelos
 tree_score = accuracy_score(y_test, tree_prediction)
-knn_score = accuracy_score(y_test, knn_prediction)
+rf_score = accuracy_score(y_test, rf_prediction)
+svc_score = accuracy_score(y_test, svc_prediction)
 
 # Pedindo entrada do usuário
 print("Informe sua entrada conforme a sequência descrita: (cada valor deve ser separado por ENTER)")
@@ -54,14 +59,20 @@ input_vector = np.array(input_vector).reshape(1, -1)
 print_board(input_raw)
 
 # Avaliando a qual modelo submeter os dados do usuário e fazendo a predição dos mesmos
-if tree_score > knn_score:
+if tree_score > rf_score and tree_score > svc_score:
     resultado = tree.predict(input_vector)
     model = "Árvore de decisão"
     accuracy = tree_score
+
+elif rf_score > tree_score and rf_score > svc_score:
+    resultado = rf.predict(input_vector)
+    model = "Random Forest"
+    accuracy = rf_score
+
 else:
-    resultado = knn.predict(input_vector)
-    model = "KNN"
-    accuracy = knn_score
+    resultado = svc.predict(input_vector)
+    model = "SVC"
+    accuracy = svc_score
 
 resultado = "x venceu" if resultado[0] == 1 else "x não venceu"
 
